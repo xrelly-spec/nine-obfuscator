0c2f5ac5c713c2c212e2fa8474bb825c2616a5ad
xrelly-spec <rendirajabpratama@gmail.com>
2026-01-19 23:09:08 +0700

    Create options.js

---
+++ options.js
@@ -0,0 +1,15 @@
+module.exports = {
+  target: "node",
+  preset: "medium",
+  stringEncoding: true,
+  stringCompression: true,
+  stringConcealing: true,
+  controlFlowFlattening: true,
+  deadCode: true,
+  renameVariables: true,
+  renameGlobals: false,
+  calculator: true,
+  hexadecimalNumbers: true,
+  dispatcher: true,
+  objectExtraction: true
+}
