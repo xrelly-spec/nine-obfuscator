a519f6e5117d5837c2b023280cecd57a876fa1db
xrelly-spec <rendirajabpratama@gmail.com>
2026-01-19 23:06:31 +0700

    Delete utils directory

--- utils/random.js
+++
@@ -1,8 +0,0 @@
-module.exports = function randomName(len = 8) {
-  const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  let out = ""
-  for (let i = 0; i < len; i++) {
-    out += chars[Math.floor(Math.random() * chars.length)]
-  }
-  return out
-}
