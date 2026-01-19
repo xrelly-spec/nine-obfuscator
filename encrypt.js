7bef48a6c23fa2d12250087ae0107dec6dc12406
xrelly-spec <rendirajabpratama@gmail.com>
2026-01-19 23:15:11 +0700

    Update encrypt.js

--- encrypt.js
+++ encrypt.js
@@ -1,5 +1,26 @@
 const JsConfuser = require("js-confuser")
+const crypto = require("crypto")
 const defaultOptions = require("./options")
+
+function buildAntiTamper() {
+  return `(function(){
+    function die(){
+      try{process.exit(1)}catch(e){while(true){}}
+    }
+    try{
+      const fs = require("fs")
+      const crypto = require("crypto")
+
+      const realPath = __filename
+      const code = fs.readFileSync(realPath,"utf8")
+      const hash = crypto.createHash("sha256").update(code).digest("hex")
+
+      const LOCK = "__HASH__"
+      if (hash !== LOCK) die()
+
+    }catch(e){die()}
+  })();`
+}
 
 module.exports = async function encrypt(code, customOptions = {}) {
   if (typeof code !== "string") {
@@ -10,11 +31,19 @@ module.exports = async function encrypt(code, customOptions = {}) {
     ...defaultOptions,
     ...customOptions
   }
+  let wrapped = `
+/* nine-obfuscator-protect */
+${buildAntiTamper(options.lockPath || "__filename__")}
+${code}
+`
 
-  try {
-    const result = await JsConfuser.obfuscate(code, options)
-    return result
-  } catch (err) {
-    throw new Error("Encrypt gagal: " + err.message)
-  }
+  const hash = crypto
+    .createHash("sha256")
+    .update(wrapped)
+    .digest("hex")
+
+  wrapped = wrapped.replace("__HASH__", hash)
+
+  const result = await JsConfuser.obfuscate(wrapped, options)
+  return result
 }
