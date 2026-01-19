33852d3047ab09d3a1cbc3f7dd866201e79a7e93
xrelly-spec <rendirajabpratama@gmail.com>
2026-01-19 22:52:10 +0700

    Update encryptor.js

--- core/encryptor.js
+++ core/encryptor.js
@@ -1,28 +1,17 @@
-function encodeString(str) {
-  const codes = []
-
-  for (let i = 0; i < str.length; i++) {
-    codes.push(str.charCodeAt(i) + 1)
-  }
-
-  return codes
-}
-
-function buildDecoder(codes) {
-  return `
-(function(){
-  var _r = [];
-  for(var i = 0; i < ${codes.length}; i++){
-    _r.push(String.fromCharCode(${codes[i]} - 1));
-  }
-  return _r.join("");
-})()
-`
-}
-
 module.exports = {
   encryptString(str) {
-    const encoded = encodeString(str)
-    return buildDecoder(encoded)
+    const out = []
+    for (let i = 0; i < str.length; i++) {
+      out.push(str.charCodeAt(i) + 1)
+    }
+    return out.join(",")
+  },
+
+  decryptRuntime(encoded) {
+    return `
+      function __nine_decode(s){
+        return s.split(",").map(c=>String.fromCharCode(c-1)).join("")
+      }
+    `
   }
 }
