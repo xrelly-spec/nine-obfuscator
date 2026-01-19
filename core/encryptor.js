25e19ffa917bb2e82c932e4bc13c33281ee22698
xrelly-spec <rendirajabpratama@gmail.com>
2026-01-19 21:38:15 +0700

    Update encryptor.js

--- core/encryptor.js
+++ core/encryptor.js
@@ -1,7 +1,28 @@
-const { xorEncode } = require("../utils/xor")
+function encodeString(str) {
+  const codes = []
+
+  for (let i = 0; i < str.length; i++) {
+    codes.push(str.charCodeAt(i) + 1)
+  }
+
+  return codes
+}
+
+function buildDecoder(codes) {
+  return `
+(function(){
+  var _r = [];
+  for(var i = 0; i < ${codes.length}; i++){
+    _r.push(String.fromCharCode(${codes[i]} - 1));
+  }
+  return _r.join("");
+})()
+`
+}
 
 module.exports = {
   encryptString(str) {
-    return xorEncode(str)
+    const encoded = encodeString(str)
+    return buildDecoder(encoded)
   }
 }
